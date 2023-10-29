996b3e2d3e7c285115b7369b14b80f6685d5c018
Senthilnathan <ssenthil1490@outlook.com>
2023-10-29 17:50:56 +0530

    about page

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import './about.css';
-
+import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
-    <section className="about container section" id='about z'></section>
-  )
-}
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
+    <div className="about__container grid">
 
-export default About
+      <div className="about__data grid">
+        <div className="about__info">
+          <p className="about__description">
+            Velit qui officia laboris mollit laboris sit nulla ipsum minim sint
+            ipsum mollit consectetur.Do elit ad amet quis Lorem adipisicing
+            nostrud irure laborum in.Irure est ullamco do nisi id eu id eu
+            veniam nisi mollit sunt eu.
+          </p>
+          <a href="" className="btn"></a>
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
+
