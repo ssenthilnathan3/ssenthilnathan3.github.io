bbcb71206a386f52f91063bcad3c31c253bf9359
Senthilnathan <ssenthil1490@outlook.com>
2024-08-04 18:55:15 +0000

    changes for routing ;) 6

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ const App = () => {
   return (
     <HashRouter>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-      {window.location.pathname === '/' ? <Navbar /> : null}
-      {window.location.pathname == '/finally' ? <BackButton/> : null}
+      {window.location.pathname === 'https://ssenthilnathan.github.io/' ? <Navbar /> : null}
+      {window.location.pathname == 'https://ssenthilnathan.github.io/#/finally' ? <BackButton/> : null}
         <Routes>
           <Route path="/" element={
             <>
