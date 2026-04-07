7ac9bddf2b0b63995d005cce7bbb799793cd9829
Senthilnathan <ssenthil1490@outlook.com>
2026-04-07 14:06:31 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,4 @@
 // Site configuration
-// Update these values to customize your blog
-
 export const SITE_TITLE = 'Senthilnathan';
 export const SITE_DESCRIPTION = 'i have some thoughts on technology, research, and ideas that i would like to put out.';
 export const AUTHOR_NAME = 'SENTHILNATHAN';
